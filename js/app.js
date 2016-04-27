ab5868e427e70a85657ca8ea3dc33f266c3c0c58
aberkow <berkowitz.clarinet@gmail.com>
2016-04-27 12:42:44 -0400

    working on yes no button functions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-
-
 var drinkIngredients = {
   strong: ["Glug of rum", "Splash of whisky", "Slug of gin"],
   salty: ["Olive on a stick", "Salt-dusted rim", "Rasher of bacon"],
@@ -9,47 +7,62 @@ var drinkIngredients = {
 };
 
 var pantryIngredients = {
-  drinks: ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"]
+  drinks: ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"],
+  food: ''
 };
 
 var menuQuestionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
 
 $(document).ready(function(){
-  bar = new Bar(barName, new Bartender('Joe'), new Pantry(pantryIngredients), new Ingredients(drinkIngredients));
-  // drinkIngredients = new Ingredients(drinkIngredients);
-  // menuQuestions = new Question(menuQuestionsArr);
-  // pantry = new Pantry(pantryIngredients);
+  //creates a new global bar object with everything we need to know about the bar.
+  bar = new Bar("Dive Bar", new Bartender('Joe', menuQuestionsArr), new Pantry(pantryIngredients), new Ingredients(drinkIngredients));
+
+  //Display the name of the bartender and ask a question for the user to answer.
+  $('.main__bartender-name').text(bar.bartender.name);
+  $('.main__question-text').text(bar.bartender.askQuestion);
+
+  $('.main__answers-button').on('click', function(){
+    if ($(this).html() === 'Yes'){
+      $('.main__answers').append('Hold on');
+    } else {
+      debugger;
+      bar.bartender.askQuestion;
+    }
+  });
 });
 
 function Bar(barName, bartender, pantry, ingredients){
+  this.userAnswer = '';
   this.barName = barName;
   this.bartender = bartender;
   this.pantry = pantry;
   this.ingredients = ingredients;
 }
 
-function Question(questionArr){
-  this.questions = questionArr;
-  //console.log(this.questions);
-  this.randomQuestion = this.questions[Math.floor(Math.random()*this.questions.length)];
-  console.log(this.randomQuestion);
-}
+// function Question(questionArr){
+//   this.questions = questionArr;
+//   this.randomQuestion = this.questions[Math.floor(Math.random()*this.questions.length)];
+//   console.log(this.randomQuestion);
+// }
 
 function Ingredients(ingredients){
   this.availableIngredients = ingredients;
-  // console.log('ingredients');
-  // console.log(this.availableIngredients);
 }
 
 function Pantry(pantryIngredients){
   this.pantryIngredients = pantryIngredients;
 }
 
-function Bartender(name){
+function Bartender(name, questionArr){
   this.name = name;
+  this.askQuestion = questionArr[Math.floor(Math.random() * questionArr.length)];
+  //this.askQuestion(questionArr);
 }
 
-//ask questions randomly without repeats, responds to y/n input
-function randmomQuestion(inputQuestionArr){
+// Bartender.prototype.askQuestion = function(questionArr){
+//   this.askQuestion = questionArr[Math.floor(Math.random() * questionArr.length)];
+// };
 
-}
+// function askQuestion(questionArr){
+//
+// }
