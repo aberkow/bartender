de96f8c35aeecadd8f929cb560f404825fbeb04e
aberkow <berkowitz.clarinet@gmail.com>
2016-05-04 22:14:11 -0400

    getting closer

--- js/app.js
+++ js/app.js
@@ -1,31 +1,8 @@
-/*
-Bar -
-Pantry - Bar
-Ingredients - Pantry - Bar
-Bartender - Ingredient - Pantry - Bar
-*/
-
-function Bar(){
-  this.barNameArr = ["The Salty Dog", "Arrrrrby's", "Handsome Jack's", "The Plank", "Treasure Island"];
-  this.barName = this.barNameArr[Math.floor(Math.random() * this.barNameArr.length)];
-  this.bartenderNameArr = ["One Eye Pete", "The Captain", "Jim.... What!? Think ye that every pirate needs a silly name!?"];
-  this.bartenderName = this.bartenderNameArr[Math.floor(Math.random() * this.bartenderNameArr.length)];
-}
-
-Bar.prototype.pantry = function(){
-  this.mainDrinksArr = ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"],
-  this.extraDrinkIngredients = {
-    strong: ["Glug of rum", "Splash of whisky", "Slug of gin"],
-    salty: ["Olive on a stick", "Salt-dusted rim", "Rasher of bacon"],
-    bitter: ["Shake of bitters", "Splash of tonic", "Twist of lemon"],
-    sweet: ["Sugar cube", "Spoonful of honey", "Splash of cola"],
-    fruity: ["Slice of orange", "Dash of cassis", "Cherry on top"]
-  }
-};
-
 var barNameArr = ["The Salty Dog", "Arrrrrby's", "Handsome Jack's", "The Plank", "Treasure Island"];
 
 var bartenderNameArr = ["One Eye Pete", "The Captain", "Jim.... What!? Think ye that every pirate needs a silly name!?"];
+
+var mainDrinksArr = ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"];
 
 var drinkIngredients = {
   strong: ["Glug of rum", "Splash of whisky", "Slug of gin"],
@@ -35,22 +12,50 @@ var drinkIngredients = {
   fruity: ["Slice of orange", "Dash of cassis", "Cherry on top"]
 };
 
-var pantryIngredients = {
-  drinks: ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"],
-  food: []
-};
+var drinkQuestionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
 
-var menuQuestionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
+function Bar(barNameArr){
+  this.barName = barNameArr[Math.floor(Math.random() * barNameArr.length)];
+}
+
+Bar.prototype.pantry = function(mainDrinksArr, extraDrinkIngredients){
+  this.pantryItems = {
+    mainDrinksArr: mainDrinksArr,
+    extraDrinkIngredients: extraDrinkIngredients
+  }
+}
+
+//try to make bartender a prototype of Person...
+// function Person(name){
+//   this.name = name;
+// }
+
+function Bartender(name, questionArr, bar){
+  //the bartender needs a bar with a pantry that has mainDrinksArr and extraDrinkIngredients object
+  this.bartenderName = name;
+  // this.questionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
+  this.askQuestion = function(questionArr){
+      var question = questionArr[Math.floor(Math.random() * questionArr.length)];
+      return question;
+  };
+  this.stuffIHave = bar.pantry(mainDrinksArr, drinkIngredients);
+}
+
+Bartender.prototype.mixDrink = function(){
+
+}
 
 $(document).ready(function(){
   //debugger;
   //creates a new global bar object with everything we need to know about the bar.
-  bar = new Bar(barNameArr, new Bartender(bartenderNameArr, menuQuestionsArr), new Pantry(pantryIngredients), new Ingredients(drinkIngredients));
+  // bar = new Bar(barNameArr, new Bartender(bartenderNameArr, menuQuestionsArr), new Pantry(pantryIngredients), new Ingredients(drinkIngredients));
+  var bar = new Bar(barNameArr);
+  var bartender = new Bartender("Jim", drinkQuestionsArr, bar);
   //Display the name of the bar, bartender, and ask a question for the user to answer.
   $('.main__welcome-text').text(bar.barName);
-  $('.main__bartender-name').append(" " + bar.bartender.name);
-
-  $('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
+  $('.main__bartender-name').append(" " + bartender.bartenderName);
+  debugger;
+  //$('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
 
   //
   $('.main__answers-button').on('click', function(){
@@ -58,49 +63,72 @@ $(document).ready(function(){
     if ($('.main__answers-button').html() === 'Yes'){
       // var customerAnswer = bar.bartender.questionArr.indexOf($('.main__question-text').text());
       // console.log(customerAnswer);
-      $('.main__answers').append('Hold on' + bar.bartender.getCustomerAnswer());
+      //$('.main__answers').append('Hold on' + bar.bartender.getCustomerAnswer());
       debugger;
     } else {
-      $('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
+      //$('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
     }
   });
 });
 
-function Bar(barNameArr, bartender, pantry, ingredients){
-  //var self = this;
+// function Bar(barNameArr, bartender, pantry, ingredients){
+//   //var self = this;
+//
+//   this.userAnswer = '';
+//   this.barNameArr = barNameArr;
+//   this.barName = barNameArr[Math.floor(Math.random() * barNameArr.length)];
+//   this.bartender = bartender;
+//   this.pantry = pantry;
+//   this.ingredients = ingredients;
+// }
+//
+// function Ingredients(ingredients){
+//   this.availableIngredients = ingredients;
+// }
+//
+// function Pantry(pantryIngredients){
+//   this.pantryIngredients = pantryIngredients;
+// }
+//
+// function Bartender(bartenderNameArr, questionArr){
+//   //self makes sure that this keeps its context
+//   var self = this;
+//   this.nameArr = bartenderNameArr;
+//   this.name = bartenderNameArr[Math.floor(Math.random() * bartenderNameArr.length)];
+//   this.questionArr = questionArr;
+//   this.askQuestion = function(questionArr){
+//     var question = questionArr[Math.floor(Math.random() * questionArr.length)];
+//     return question;
+//   };
+//
+//   this.getCustomerAnswer = function(){
+//     return self.questionArr.indexOf($('.main__question-text').text());
+//   };
+//
+//   this.makeDrink = function(drinkIngredients, pantryIngredients){
+//     //match the customerAnswer index to the index of the drink ingredients plus one random pantry ingredient.
+//   };
+// }
 
-  this.userAnswer = '';
-  this.barNameArr = barNameArr;
-  this.barName = barNameArr[Math.floor(Math.random() * barNameArr.length)];
-  this.bartender = bartender;
-  this.pantry = pantry;
-  this.ingredients = ingredients;
-}
+// var drinkIngredients = {
+//   strong: ["Glug of rum", "Splash of whisky", "Slug of gin"],
+//   salty: ["Olive on a stick", "Salt-dusted rim", "Rasher of bacon"],
+//   bitter: ["Shake of bitters", "Splash of tonic", "Twist of lemon"],
+//   sweet: ["Sugar cube", "Spoonful of honey", "Splash of cola"],
+//   fruity: ["Slice of orange", "Dash of cassis", "Cherry on top"]
+// };
+//
+// var pantryIngredients = {
+//   drinks: ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"],
+//   food: []
+// };
+//
+// var menuQuestionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
 
-function Ingredients(ingredients){
-  this.availableIngredients = ingredients;
-}
 
-function Pantry(pantryIngredients){
-  this.pantryIngredients = pantryIngredients;
-}
-
-function Bartender(bartenderNameArr, questionArr){
-  //self makes sure that this keeps its context
-  var self = this;
-  this.nameArr = bartenderNameArr;
-  this.name = bartenderNameArr[Math.floor(Math.random() * bartenderNameArr.length)];
-  this.questionArr = questionArr;
-  this.askQuestion = function(questionArr){
-    var question = questionArr[Math.floor(Math.random() * questionArr.length)];
-    return question;
-  };
-
-  this.getCustomerAnswer = function(){
-    return self.questionArr.indexOf($('.main__question-text').text());
-  };
-
-  this.makeDrink = function(drinkIngredients, pantryIngredients){
-    //match the customerAnswer index to the index of the drink ingredients plus one random pantry ingredient.
-  };
-}
+// function Drink(){
+//   this.name = {};
+//   this.ingredients = {};
+//   this.price = " ";
+//   this.mixDrink = function(this.ingredients){}
+// }
