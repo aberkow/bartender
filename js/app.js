4e460df30456e57ba48aebdb533236b9a7a2595b
aberkow <berkowitz.clarinet@gmail.com>
2016-05-05 15:45:36 -0400

    much improved inheritance btwn Person and Bartender

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var bartender;
+var bar;
 var barNameArr = ["The Salty Dog", "Arrrrrby's", "Handsome Jack's", "The Plank", "Treasure Island"];
 
 var bartenderNameArr = ["One Eye Pete", "The Captain", "Jim.... What!? Think ye that every pirate needs a silly name!?"];
@@ -5,11 +7,11 @@ var bartenderNameArr = ["One Eye Pete", "The Captain", "Jim.... What!? Think ye 
 var mainDrinksArr = ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"];
 
 var drinkIngredients = {
-  strong: ["Glug of rum", "Splash of whisky", "Slug of gin"],
-  salty: ["Olive on a stick", "Salt-dusted rim", "Rasher of bacon"],
-  bitter: ["Shake of bitters", "Splash of tonic", "Twist of lemon"],
-  sweet: ["Sugar cube", "Spoonful of honey", "Splash of cola"],
-  fruity: ["Slice of orange", "Dash of cassis", "Cherry on top"]
+  strong: ["a glug of rum", "a splash of whisky", "a slug of gin"],
+  salty: ["an olive on a stick", "a salt-dusted rim", "a rasher of bacon"],
+  bitter: ["a shake of bitters", "a splash of tonic", "a twist of lemon"],
+  sweet: ["a sugar cube", "a spoonful of honey", "a splash of cola"],
+  fruity: ["a slice of orange", "a dash of cassis", "a cherry on top"]
 };
 
 var drinkQuestionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
@@ -19,43 +21,43 @@ function Bar(barNameArr){
 }
 
 Bar.prototype.pantry = function(mainDrinksArr, extraDrinkIngredients){
-  this.pantryItems = {
+  return this.pantryItems = { //creates object and returns it at the same time.
     mainDrinksArr: mainDrinksArr,
     extraDrinkIngredients: extraDrinkIngredients
   }
 }
 
 //try to make bartender a prototype of Person...
-// function Person(name){
-//   this.name = name;
-// }
+function Person(nameArr){
+  this.name = nameArr[Math.floor(Math.random() * nameArr.length)];
+}
 
-function Bartender(name, questionArr, bar){
-  //the bartender needs a bar with a pantry that has mainDrinksArr and extraDrinkIngredients object
-  this.bartenderName = name;
-  // this.questionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
-  this.askQuestion = function(questionArr){
+function Bartender(questionArr, bar){
+  this.askQuestion = function(){
       var question = questionArr[Math.floor(Math.random() * questionArr.length)];
       return question;
   };
   this.stuffIHave = bar.pantry(mainDrinksArr, drinkIngredients);
 }
 
-Bartender.prototype.mixDrink = function(){
+Bartender.prototype = new Person(bartenderNameArr);
 
+Bartender.prototype.mixDrink = function(){
+  console.log("Here's a drink! " + bar.pantryItems.mainDrinksArr[1] + " and a " + bar.pantryItems.extraDrinkIngredients.strong[0]);
 }
 
 $(document).ready(function(){
   //debugger;
   //creates a new global bar object with everything we need to know about the bar.
   // bar = new Bar(barNameArr, new Bartender(bartenderNameArr, menuQuestionsArr), new Pantry(pantryIngredients), new Ingredients(drinkIngredients));
-  var bar = new Bar(barNameArr);
-  var bartender = new Bartender("Jim", drinkQuestionsArr, bar);
+  bar = new Bar(barNameArr);
+  bartender = new Bartender(drinkQuestionsArr, bar);
   //Display the name of the bar, bartender, and ask a question for the user to answer.
   $('.main__welcome-text').text(bar.barName);
-  $('.main__bartender-name').append(" " + bartender.bartenderName);
-  debugger;
+  $('.main__bartender-name').append(" " + bartender.name);
+  //debugger;
   //$('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
+  $('.main__question-text').text(bartender.askQuestion());
 
   //
   $('.main__answers-button').on('click', function(){
@@ -64,7 +66,7 @@ $(document).ready(function(){
       // var customerAnswer = bar.bartender.questionArr.indexOf($('.main__question-text').text());
       // console.log(customerAnswer);
       //$('.main__answers').append('Hold on' + bar.bartender.getCustomerAnswer());
-      debugger;
+      //debugger;
     } else {
       //$('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
     }
