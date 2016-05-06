9faed3ad77e969546164b054f1977823014c6047
aberkow <berkowitz.clarinet@gmail.com>
2016-05-05 21:46:06 -0400

    inheritance btwn Person and Bartender worked out. Added getAndCompareAnswer and mixAndServe functions.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var barNameArr = ["The Salty Dog", "Arrrrrby's", "Handsome Jack's", "The Plank",
 
 var bartenderNameArr = ["One Eye Pete", "The Captain", "Jim.... What!? Think ye that every pirate needs a silly name!?"];
 
-var mainDrinksArr = ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"];
+var mainDrinksArr = ["Rum", "Whisky", "Gin", "Tonic", "Bourbon", "Water"];
 
 var drinkIngredients = {
   strong: ["a glug of rum", "a splash of whisky", "a slug of gin"],
@@ -33,6 +33,7 @@ function Person(nameArr){
 }
 
 function Bartender(questionArr, bar){
+  this.questions = questionArr;
   this.askQuestion = function(){
       var question = questionArr[Math.floor(Math.random() * questionArr.length)];
       return question;
@@ -42,95 +43,56 @@ function Bartender(questionArr, bar){
 
 Bartender.prototype = new Person(bartenderNameArr);
 
-Bartender.prototype.mixDrink = function(){
-  console.log("Here's a drink! " + bar.pantryItems.mainDrinksArr[1] + " and a " + bar.pantryItems.extraDrinkIngredients.strong[0]);
+Bartender.prototype.mixDrinkAndServe = function(userAnswer){
+  var mainDrinks = bar.pantryItems.mainDrinksArr;
+  var randomDrink = mainDrinks[Math.floor(Math.random() * mainDrinks.length)];
+  var userChoice = userAnswer;
+  console.log("Here's a drink! " + randomDrink + " and a " + userChoice);
 }
 
 $(document).ready(function(){
-  //debugger;
-  //creates a new global bar object with everything we need to know about the bar.
-  // bar = new Bar(barNameArr, new Bartender(bartenderNameArr, menuQuestionsArr), new Pantry(pantryIngredients), new Ingredients(drinkIngredients));
   bar = new Bar(barNameArr);
   bartender = new Bartender(drinkQuestionsArr, bar);
+
   //Display the name of the bar, bartender, and ask a question for the user to answer.
   $('.main__welcome-text').text(bar.barName);
   $('.main__bartender-name').append(" " + bartender.name);
-  //debugger;
-  //$('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
   $('.main__question-text').text(bartender.askQuestion());
 
-  //
+  //match the user's choice to the text or index???
   $('.main__answers-button').on('click', function(){
     //var customerAnswer;
-    if ($('.main__answers-button').html() === 'Yes'){
-      // var customerAnswer = bar.bartender.questionArr.indexOf($('.main__question-text').text());
-      // console.log(customerAnswer);
-      //$('.main__answers').append('Hold on' + bar.bartender.getCustomerAnswer());
-      //debugger;
+    if ($(this).html() === 'Yes'){
+      bartender.mixDrinkAndServe(getAndCompareAnswer());
+      console.log(getAndCompareAnswer());
     } else {
-      //$('.main__question-text').text(bar.bartender.askQuestion(menuQuestionsArr));
+      $('.main__question-text').text(bartender.askQuestion());
     }
   });
 });
 
-// function Bar(barNameArr, bartender, pantry, ingredients){
-//   //var self = this;
-//
-//   this.userAnswer = '';
-//   this.barNameArr = barNameArr;
-//   this.barName = barNameArr[Math.floor(Math.random() * barNameArr.length)];
-//   this.bartender = bartender;
-//   this.pantry = pantry;
-//   this.ingredients = ingredients;
-// }
-//
-// function Ingredients(ingredients){
-//   this.availableIngredients = ingredients;
-// }
-//
-// function Pantry(pantryIngredients){
-//   this.pantryIngredients = pantryIngredients;
-// }
-//
-// function Bartender(bartenderNameArr, questionArr){
-//   //self makes sure that this keeps its context
-//   var self = this;
-//   this.nameArr = bartenderNameArr;
-//   this.name = bartenderNameArr[Math.floor(Math.random() * bartenderNameArr.length)];
-//   this.questionArr = questionArr;
-//   this.askQuestion = function(questionArr){
-//     var question = questionArr[Math.floor(Math.random() * questionArr.length)];
-//     return question;
-//   };
-//
-//   this.getCustomerAnswer = function(){
-//     return self.questionArr.indexOf($('.main__question-text').text());
-//   };
-//
-//   this.makeDrink = function(drinkIngredients, pantryIngredients){
-//     //match the customerAnswer index to the index of the drink ingredients plus one random pantry ingredient.
-//   };
-// }
-
-// var drinkIngredients = {
-//   strong: ["Glug of rum", "Splash of whisky", "Slug of gin"],
-//   salty: ["Olive on a stick", "Salt-dusted rim", "Rasher of bacon"],
-//   bitter: ["Shake of bitters", "Splash of tonic", "Twist of lemon"],
-//   sweet: ["Sugar cube", "Spoonful of honey", "Splash of cola"],
-//   fruity: ["Slice of orange", "Dash of cassis", "Cherry on top"]
-// };
-//
-// var pantryIngredients = {
-//   drinks: ["Rum", "Whisky", "Gin", "Tonic", "Cola", "Water"],
-//   food: []
-// };
-//
-// var menuQuestionsArr = ["Do ye like yer drinks strong?", "Do ye like it with a salty tang?", "Are ye a lubber who likes it bitter?", "Would you like a little sweetness with yer poison?", "Are ye one for a fruity finish?"];
-
-
-// function Drink(){
-//   this.name = {};
-//   this.ingredients = {};
-//   this.price = " ";
-//   this.mixDrink = function(this.ingredients){}
-// }
+//match the index of the user's choice based on the text of the question to an appropriate/random drink ingredient.
+var getAndCompareAnswer = function(){
+  var userAnswerIndex = (drinkQuestionsArr.indexOf($('.main__question-text').text()));
+  var bartenderChoices = bartender.stuffIHave.extraDrinkIngredients;
+  switch (userAnswerIndex) {
+    case 0:
+      return bartenderChoices.strong[Math.floor(Math.random() * bartenderChoices.strong.length)];
+      break;
+    case 1:
+      return bartenderChoices.salty[Math.floor(Math.random() * bartenderChoices.salty.length)];
+      break;
+    case 2:
+      return bartenderChoices.bitter[Math.floor(Math.random() * bartenderChoices.bitter.length)];
+      break;
+    case 3:
+      return bartenderChoices.sweet[Math.floor(Math.random() * bartenderChoices.sweet.length)];
+      break;
+    case 4:
+      return bartenderChoices.fruity[Math.floor(Math.random() * bartenderChoices.fruity.length)];
+      break;
+    default:
+      return "Mead";
+      break;
+  }
+}
